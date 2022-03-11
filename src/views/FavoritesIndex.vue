30d7e3ac60a36fe5f29db27fa9ac8b33cc009162
Alex Krueger <alexckrueger@gmail.com>
2022-03-11 13:21:43 -0600

    displays info on favorites index

--- src/views/FavoritesIndex.vue
+++ src/views/FavoritesIndex.vue
@@ -6,10 +6,24 @@ export default {
     return {
       message: "Cows are Friends.tm",
       message2: "FavoritesIndex",
+      user: {},
+      favorites: [],
     };
   },
   created: function () {
-    axios.get(`http://localhost:3000/favorites`).then((response) => console.log(response.data));
+    axios
+      .get("/users/me")
+      .then((response) => {
+        this.user = response.data;
+      })
+      .catch((error) => {
+        if (error.response.status == 401) {
+          this.$router.push("/login");
+        }
+      });
+    axios.get(`http://localhost:3000/favorites`).then((response) => {
+      this.favorites = response.data;
+    });
   },
   methods: {},
 };
@@ -19,6 +33,14 @@ export default {
   <div class="home">
     <h1>{{ message }}</h1>
     <h2>{{ message2 }}</h2>
+    <h1>{{ user.name }} Favorites</h1>
+    <div v-for="favorite in favorites" v-bind:key="favorite.id">
+      <h3>{{ favorite.business.name }}</h3>
+      <p>{{ favorite.business.categories.join(", ") }}</p>
+      <img :src="favorite.business.image_url" alt="" />
+      <br />
+      <router-link :to="`/businesses/${favorite.business.id}`">To Restaurant Page</router-link>
+    </div>
   </div>
 </template>
 
