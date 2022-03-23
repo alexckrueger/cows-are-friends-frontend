fa067ec6694b297624d6f418bece1a5830444bac
Alex Krueger <alexckrueger@gmail.com>
2022-03-23 15:30:13 -0500

    attempts to fix map timing issue on deployment

--- src/views/BusinessesShow.vue
+++ src/views/BusinessesShow.vue
@@ -65,21 +65,6 @@ export default {
     redirectToReview: function (business) {
       this.$router.push(`/reviews/new?id=${business.id}`);
     },
-    setupLeafletMap: function () {
-      var mapboxKey = process.env.VUE_APP_MAPBOX_API_KEY;
-      this.mapDiv = L.map(this.$refs["mapElement"]).setView(this.center, 15);
-      L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution:
-          'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: "mapbox/streets-v11",
-        accessToken: mapboxKey,
-      }).addTo(this.mapDiv);
-      var marker = L.marker([this.business.coordinates.latitude, this.business.coordinates.longitude], {
-        icon: this.myIcon,
-      }).bindPopup(this.business.name);
-      marker.addTo(this.mapDiv);
-    },
     onInit: () => {
       console.log("lightGallery has been initialized");
     },
@@ -112,7 +97,19 @@ export default {
     },
   },
   mounted: function () {
-    this.setupLeafletMap();
+    var mapboxKey = process.env.VUE_APP_MAPBOX_API_KEY;
+    this.mapDiv = L.map(this.$refs["mapElement"]).setView(this.center, 15);
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution:
+        'Map data (c) <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      accessToken: mapboxKey,
+    }).addTo(this.mapDiv);
+    var marker = L.marker([this.business.coordinates.latitude, this.business.coordinates.longitude], {
+      icon: this.myIcon,
+    }).bindPopup(this.business.name);
+    marker.addTo(this.mapDiv);
   },
 };
 </script>
